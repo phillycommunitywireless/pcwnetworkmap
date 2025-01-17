bb46d5bfc479472c29b500136398ee7b5121c03f
Josh Leslie <hello@joshmleslie.com>
2025-01-17 15:05:33 -0500

    swap min/max colors for steps add supporting step function fix expression config to handle undef value fallback

--- js/util.js
+++ js/util.js
@@ -14,40 +14,62 @@ export const getRandomColor = () => {
 	return color;
 };
 
+// https://stackoverflow.com/a/68805895/9807140
+const getIntervals = (
+	min,
+	max,
+	count,
+	end = min + Math.ceil((max - min + 1) / count) - 1
+) => {
+	if (count < 1) {
+		return [];
+	}
+	return [[min, end], ...getIntervals(end + 1, max, count - 1)];
+};
+
 /**
  * Generates a random color from geojson data for 'fill-color' prop
  *
  * @param {GeoJSON} data
- * @param {string} data_id
- * @param {string} [minColor] - default red
- * @param {string} [maxColor] - default blue
- * @returns {string[]}
+ * @param {string} value_id
+ * @param {string} [undefColor] - default gray #80
+ * @param {string[]} [colorSteps] - colorSteps, defaults red -> green
  */
 export const createRangeColorExpression = (
 	data,
-	data_id,
-	minColor = '#ff0000',
-	maxColor = '#0000ff'
+	value_id,
+	undefColor = '#808080',
+	colorSteps = [
+		'#ff0000', // Red
+		'#ff8000', // Orange
+		'#ffff00', // Yellow
+		'#80ff00', // Light green
+		'#00ff00', // Green
+	]
 ) => {
-	if (!data?.features?.[0]?.properties?.[data_id]) {
-		throw new ReferenceError(
-			"Feature data doesn't contain the expected data_id"
-		);
+	if (!data?.features?.[0]?.properties?.[value_id]) {
+		throw ReferenceError("Feature data doesn't contain the expected value_id");
 	}
 	const incomeValues = data.features.map(
-		(feature) => feature.properties[data_id]
+		(feature) => feature.properties[value_id]
 	);
-	const minIncome = Math.min(...incomeValues);
-	const maxIncome = Math.max(...incomeValues);
+	const intervals = getIntervals(
+		Math.min(...incomeValues),
+		Math.max(...incomeValues),
+		colorSteps.length
+	);
+	const steps = intervals.reduce(
+		(acc, [_, intervalMax], i) => acc.concat(intervalMax, colorSteps[i]),
+		[]
+	);
 
 	return [
 		'interpolate',
 		['linear'],
-		['to-number', ['get', data_id]],
-		minIncome,
-		minColor,
-		maxIncome,
-		maxColor,
+		['coalesce', ['to-number', ['get', value_id]], -1],
+		-1,
+		undefColor,
+		...steps,
 	];
 };
 
