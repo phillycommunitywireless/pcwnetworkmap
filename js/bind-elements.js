a53ad5c149034983fa5b92d1c0530be2dacfea43
Josh Leslie <hello@joshmleslie.com>
2025-01-27 15:21:27 -0500

    fix 32 swap conflicting easing call for css remove listener on whole sidebar and instead only on button fix hidden sidebar stretching body width via overflow: hidden

--- js/bind-elements.js
+++ js/bind-elements.js
@@ -8,13 +8,7 @@ import { FlyToDefaults, NavBookmarks } from './const.js';
 
 export const toggleSidebar = () => {
 	const sidebar = document.getElementById('right-sidebar');
-	const isCollapsed = sidebar.classList.toggle('collapsed');
-	const padding = isCollapsed ? 0 : 300;
-	// Use `map.easeTo()` with a padding option to adjust the map's center accounting for the position of sidebars.
-	map.easeTo({
-		padding: {right: padding},
-		duration: 1000, // In ms. This matches the CSS transition duration property.
-	});
+	sidebar.classList.toggle('collapsed');
 };
 
 const navigateToBookmark = (bookmarkId) => {
@@ -24,13 +18,15 @@ const navigateToBookmark = (bookmarkId) => {
 	}
 	map.flyTo({
 		...FlyToDefaults,
-		...NavBookmarks[bookmarkId]
+		...NavBookmarks[bookmarkId],
 	});
+	toggleSidebar();
 };
 
 export default () => {
-	const rightSidebar = document.getElementById('right-sidebar');
-	rightSidebar.addEventListener('click', toggleSidebar);
+	document
+		.getElementById('sidebar-toggle')
+		.addEventListener('click', toggleSidebar);
 
 	// visibility bindings
 	document
